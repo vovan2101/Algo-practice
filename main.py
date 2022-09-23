60af456400c988c46722a1fccd81fe0dd1ea49ce
Vladimir <veliseykin2000@gmail.com>
2022-09-22 21:34:19 -0700

    +1 example of algo

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-a = 'lilila'
+# Алгоритм Кнута-Морриса-Пратта(КПМ)
 
-p = [0] * len(a)
+
+t = 'лилила'
+
+p = [0] * len(t)
 j = 0
 i = 1
 
-while i < len(a):
-    if a[j] == a[i]:
+while i < len(t):
+    if t[j] == t[i]:
         p[i] = j + 1
         i += 1
         j += 1
@@ -17,3 +20,25 @@ while i < len(a):
             j = p[j - 1]
 
 print(p)
+
+a = "лилилось лилилась"
+m = len(t)
+n = len(a)
+
+i = 0
+j = 0
+while i < n:
+    if a[i] == t[j]:
+        i += 1
+        j += 1
+        if j == m:
+            print('Образ найден')
+            break
+    else: 
+        if j > 0:
+            j = p[j-1]
+        else:
+             i += 1
+if i == n:
+    print("Образ не найден")
+    
